0d9937f427e2503f434abe0e6a25f7a62fc8f176
Diegucho7 <87243460+Diegucho7@users.noreply.github.com>
2024-02-27 01:20:25 -0500

    listo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
 
     production: false,
-    base_url : 'http://localhost:3000/api'
+    // base_url : 'http://localhost:3000/api'
+    base_url : 'https://academia-backend-production.up.railway.app/api'
 
 };
